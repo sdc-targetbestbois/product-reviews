6c7d9b7455b8c101592e133c548865afadfd4a79
root <root@DESKTOP-3F7B0P6.localdomain>
2020-05-05 15:09:14 -0500

    Added Review component, hardcoded to render 3 reviews in ReviewList

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,13 +1,10 @@
 import React from "react";
-import Review from "./Review.js"
 
 
 const ReviewList = (props) => {
   return (
     <div>
-      <Review />
-      <Review />
-      <Review />
+      This is review list
     </div>
 
   )
