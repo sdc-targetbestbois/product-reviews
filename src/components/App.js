04cd4bde0ae182219e13f92d4a8f3a2a061a540f
root <root@DESKTOP-3F7B0P6.localdomain>
2020-05-08 12:17:51 -0500

    Combine Header and App. Have each unqiue click handler for seperate dropdown elements working.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
 import Axios from "axios";
-import Header from "./Header.js";
 import ReviewList from "./ReviewList.js";
+import css from "../styles/header.css";
+import Stars from "./Stars.js"
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
       allReviews: [],
+      currentReviews: [],
     };
   }
+
+  handleClickAll(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+  }
+  handleClick1(e) {
+    e.preventDefault();
+    console.log('The link was clicked 1.');
+  }
+  handleClick2(e) {
+    e.preventDefault();
+    console.log('The link was clicked 2.');
+  }
+  handleClick3(e) {
+    e.preventDefault();
+    console.log('The link was clicked 3.');
+  }
+  handleClick4(e) {
+    e.preventDefault();
+    console.log('The link was clicked 4.');
+  }
+  handleClick5(e) {
+    e.preventDefault();
+    console.log('The link was clicked 5.');
+  }
+
 
   componentDidMount() {
     Axios.get('/api/reviews')
@@ -24,7 +52,24 @@ export default class App extends Component {
   render() {
     return (
       <div class="mainContainer">
-        <Header />
+
+        {/* HEADER HERE */}
+        <div class="header">
+            Guest Rating and Reviews
+          <h1> 4.8 </h1>
+          <Stars />
+            <div class="dropdown">
+              <button class="dropbtn">Filter by</button>
+                <div class="dropdown-content">
+                  <a href="#" onClick={this.handleClickAll}> All Reviews</a>
+                  <a href="#" onClick={this.handleClick1}> 1 Stars</a>
+                  <a href="#" onClick={this.handleClick2}> 2 Stars</a>
+                  <a href="#" onClick={this.handleClick3}> 3 Stars</a>
+                  <a href="#" onClick={this.handleClick4}> 4 Stars</a>
+                  <a href="#" onClick={this.handleClick5}> 5 Stars</a>
+                </div>
+            </div>
+        </div>
         <ReviewList reviews={this.state.allReviews}/>
       </div>
     );
