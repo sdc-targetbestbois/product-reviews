2b727110aca8cee0f816b5004354a81f55ec4731
root <root@DESKTOP-3F7B0P6.localdomain>
2020-05-16 20:18:36 -0500

    Enchance the reviews look

--- src/components/Review.js
+++ src/components/Review.js
@@ -7,17 +7,23 @@ const Review = ({review}) => {
   return (
     <div>
       <h4 className="reviewTitle">{review.title}</h4>
-      <div className="stars">
-        <StarRatings
-        starDimension='18px'
-        starSpacing='0px'
-        rating={review.stars}
-        starRatedColor='yellow'
-        numberOfStars={5}
-        />
-        {review.stars > 2 ? <p>would recommend</p> : <p>Would not recommend</p>}
+      <div className="recommendBar">
+        <div className="stars">
+          <StarRatings
+          starDimension='18px'
+          starSpacing='0px'
+          rating={review.stars}
+          starRatedColor='yellow'
+          numberOfStars={5}
+          />
+        </div>
+
+        <div>
+          {review.stars > 2 ? <span>| Would recommend</span> : <span>| Would not recommend</span>}
+        </div>
+
       </div>
-      <p className="userLine">{review.user} - 4 days ago, Verified purchaser</p>
+      <p className="userLine">{review.user} - {Math.floor(Math.random() * (31 - 1) + 1)} days ago, Verified purchaser</p>
       <p>{review.reviewText}</p>
     </div>
 
